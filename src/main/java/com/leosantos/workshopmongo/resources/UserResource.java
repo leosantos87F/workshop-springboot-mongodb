ccb405c8d71ffb8a0c2aba68096ab6a34c81b323
Leonardo Santos <leosantos.vinte@gmail.com>
2023-01-08 16:32:17 -0300

    Get a user by id

--- src/main/java/com/leosantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/leosantos/workshopmongo/resources/UserResource.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leosantos.workshopmongo.domain.User;
@@ -29,4 +31,11 @@ public class UserResource implements Serializable {
 	    List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
 }
